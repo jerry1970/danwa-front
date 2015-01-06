8afb5a2788a7efde7da9f90e48628d62e8f8e5db
devvoh <hello@devvoh.com>
2015-01-06 08:19:35 +0100

    FIX: front-end

--- README.md
+++ README.md
@@ -6,5 +6,6 @@ danwa front-end, using backbone
 to use
 ======
 
-1. run 'bower update' to get the bower compnents
-2. run './comminist' to prepare the assets css/js files
+1. install npm and bower
+2. run 'bower update' to get the bower compnents
+3. run './comminist' to prepare the assets css/js files
--- public/common.js
+++ public/common.js
@@ -3,7 +3,8 @@
 // AJAX PRE-FILTER TO SET URL CORRECTLY
     $.ajaxPrefilter( function( options, originalOptions, jqXHR ) {
         //options.url = '..' + options.url;
-        options.url = 'http://api.danwa.net' + options.url;
+        //options.url = 'http://api.danwa.net' + options.url;
+        options.url = 'http://localhost/danwa-api/public' + options.url;
     });
 
 // ROUTES INITIALIZE
@@ -14,10 +15,18 @@
     })
     var router = new Router();
 
-// MODELS
+// MODELS & COLLECTIONS
     var Users = Backbone.Collection.extend({
         url: '/v1/users'
-    })
+    });
+    var AllPosts = Backbone.Collection.extend({
+	initialize: function(models, options) {
+	    this.url = '/v1/consolidated/' + options.id + '/allposts';
+	}
+    });
+    var Posts = Backbone.Collection.extend({
+	url: '/v1/posts'
+    });
     
 // VIEWS
     var PageHome = Backbone.View.extend({
@@ -26,21 +35,22 @@
             _.bindAll(this, 'render');
         },
         render: function() {
-            var users = new Users();
-            users.fetch({
-                success: function(users) {
-                    var users = users.shift();
-                    var template = _.template($('#template-user-list').html());
+            var allPosts = new AllPosts([], {id: '54a31c8af2b08'});
+            allPosts.fetch({
+                success: function(posts) {
+                    var posts = posts.shift();
+                    var template = _.template($('#template-post-list').html());
 
                     $('.page').html(template({
-                        'users': users.attributes.data.users}
+                        'posts': posts.attributes.data.posts}
                     ));
                     $('.page').animate({
                         'opacity': 1
                     });
                 },
                 error: function() {
-                    console.log('ERROR',arguments)
+                    // arguments[1] is what we want to pass along
+                    addNotification({type: 'danger', text: 'api not available'});
                 }
             });
         }
@@ -54,3 +64,17 @@
 
 // START BACKBONE HISTORY
 Backbone.history.start();
+
+// DOCUMENT READY
+$(function() {
+    $('.notifications').on('click', '.notification .close', function() {
+	$(this).parent().remove();
+    });
+});
+
+// UTILITY FUNCTIONS
+function addNotification(notification) {
+    var template = _.template($('#template-notification').html());
+    var templateHtml = template({notification: notification});
+    $('.notifications').prepend(templateHtml);
+}
