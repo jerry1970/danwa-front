48a71a952696b85a5a3e732a54e3b355ea77f946
devvoh <hello@devvoh.com>
2015-01-15 00:55:25 +0100

    ADD: 404 route & handling in common.js ADD: modal popovers (start) ADD: font-awesome & icons in header FIX: css

--- public/assets/js/common.js
+++ public/assets/js/common.js
@@ -10,7 +10,8 @@
 // ROUTES INITIALIZE
     var Router = Backbone.Router.extend({
         routes: {
-            '': 'home'
+            '': 'home',
+            '404': 'notFound'
         }
     })
     var router = new Router();
@@ -30,7 +31,6 @@
     
 // VIEWS
     var PageHome = Backbone.View.extend({
-        el: $('.page'),
         initialize: function(){
             _.bindAll(this, 'render');
         },
@@ -38,43 +38,95 @@
             var allPosts = new AllPosts([], {id: '54b6e115d6456'});
             allPosts.fetch({
                 success: function(posts) {
+                    // by shifting we get the actual model
                     var posts = posts.shift();
                     var template = _.template($('#template-post-list').html());
 
                     $('.page').html(template({
-                        'posts': posts.attributes.data.posts}
-                    ));
+                        'posts': posts.attributes.data.posts
+                    }));
                     $('.page').animate({
                         'opacity': 1
                     });
                 },
                 error: function() {
-                    // arguments[1] is what we want to pass along
                     addNotification({type: 'danger', text: 'api not available'});
                 }
             });
         }
     });
     var pageHome = new PageHome();
+
+    var NotFound = Backbone.View.extend({
+        initialize: function(){
+            _.bindAll(this, 'render');
+        },
+        render: function() {
+            setTimeout(function() {
+        	addNotification({type: 'warning', text: 'page not found'});
+            }, 500);
+        }
+    });
+    var notFound = new NotFound();
     
 // ROUTE HANDLING
     router.on('route:home', function() {
         pageHome.render();
     });
+    router.on('route:notFound', function() {
+        notFound.render();
+    });
 
 // START BACKBONE HISTORY
-Backbone.history.start();
+if (!Backbone.history.start()) router.navigate('404', {trigger:true});
 
 // DOCUMENT READY
 $(function() {
     $('.notifications').on('click', '.notification .close', function() {
 	$(this).parent().remove();
     });
+    
+    $('.modal-popover-link').on('click', function() {
+	openModalPopover($(this));
+    });
 });
 
 // UTILITY FUNCTIONS
 function addNotification(notification) {
+    console.log($('#template-notification').html());
     var template = _.template($('#template-notification').html());
     var templateHtml = template({notification: notification});
     $('.notifications').prepend(templateHtml);
 }
+
+function openModalPopover(element) {
+    var source = $('#' + element.data('source-id'));
+
+    if ($('.modal-popover.' + element.data('source-id')).length > 0) {
+	$('.modal-popover').remove();
+	return;
+    }
+    
+    if (source.length > 0) {
+	$('.modal-popover').remove();
+	var popover = $('.modal-popover-prototype').clone();
+	
+	// do most of the settings, but keep it hidden until it's been appended
+	popover
+	    .removeClass('.modal-popover-prototype')
+	    .addClass('modal-popover ' + element.data('source-id'))
+	    .css({
+		top: element.offset().top + element.height() + 15
+	    })
+	    .html(source.html())
+	    .hide();
+	
+	$('body').append(popover);
+	
+	// now that it's appended, we can get the width, set the left offset & show
+	popover
+	   .css({
+		left: element.offset().left - (popover.width() / 2)
+	   }).show();
+    }
+} 
