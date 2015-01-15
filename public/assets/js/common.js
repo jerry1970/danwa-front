a4219a2c9e938a46cf0192e31f4df4d049ca2eb4
Robin de Graaf <r.degraaf@e-matcher.com>
2015-01-15 14:33:44 +0100

    FIX: made global object with targets (targets.x) FIX: size of elements made more consistent FIX: removed inner padding on svg element, is now no longer square, but no longer needs negative margin to line up FIX: modal popovers 'arrow' resized & repositioned

--- public/assets/js/common.js
+++ public/assets/js/common.js
@@ -1,5 +1,11 @@
 // danwa common.js, 2014
 
+// SET SOME DEFAULT TARGET ELEMENTS
+var targets = {
+    page: $('.page'),
+    header: $('.site-header')
+}
+
 // AJAX PRE-FILTER TO SET URL CORRECTLY
     $.ajaxPrefilter( function( options, originalOptions, jqXHR ) {
         //options.url = '..' + options.url;
@@ -21,12 +27,12 @@
         url: '/v1/users'
     });
     var AllPosts = Backbone.Collection.extend({
-	initialize: function(models, options) {
-	    this.url = '/v1/consolidated/' + options.id + '/allposts';
-	}
+    initialize: function(models, options) {
+        this.url = '/v1/consolidated/' + options.id + '/allposts';
+    }
     });
     var Posts = Backbone.Collection.extend({
-	url: '/v1/posts'
+        url: '/v1/posts'
     });
     
 // VIEWS
@@ -42,10 +48,10 @@
                     var posts = posts.shift();
                     var template = _.template($('#template-post-list').html());
 
-                    $('.page').html(template({
+                    targets.page.html(template({
                         'posts': posts.attributes.data.posts
                     }));
-                    $('.page').animate({
+                    targets.page.animate({
                         'opacity': 1
                     });
                 },
@@ -81,16 +87,17 @@ if (!Backbone.history.start()) router.navigate('404', {trigger:true});
 // DOCUMENT READY
 $(function() {
     $('.notifications').on('click', '.notification .close', function() {
-	$(this).parent().remove();
+        $(this).parent().remove();
     });
     
     $('.modal-popover-link').on('click', function() {
-	openModalPopover($(this));
+        openModalPopover($(this));
     });
 });
 
 // UTILITY FUNCTIONS
 function addNotification(notification) {
+    // we need the timeout to pull it from the flow, to make sure that the template has rendered
     setTimeout(function() {
         var template = _.template($('#template-notification').html());
         var templateHtml = template({notification: notification});
@@ -102,30 +109,29 @@ function openModalPopover(element) {
     var source = $('#' + element.data('source-id'));
 
     if ($('.modal-popover.' + element.data('source-id')).length > 0) {
-	$('.modal-popover').remove();
-	return;
+        $('.modal-popover').remove();
+        return;
     }
     
     if (source.length > 0) {
-	$('.modal-popover').remove();
-	var popover = $('.modal-popover-prototype').clone();
-	
-	// do most of the settings, but keep it hidden until it's been appended
-	popover
-	    .removeClass('.modal-popover-prototype')
-	    .addClass('modal-popover ' + element.data('source-id'))
-	    .css({
-		top: element.offset().top + element.height() + 15
-	    })
-	    .html(source.html())
-	    .hide();
-	
-	$('body').append(popover);
-	
-	// now that it's appended, we can get the width, set the left offset & show
-	popover
-	   .css({
-		left: element.offset().left - (popover.width() / 2)
-	   }).show();
+        $('.modal-popover').remove();
+        var popover = $('.modal-popover-prototype').clone();
+        
+        // do most of the settings, but keep it hidden until it's been appended
+        popover.removeClass('.modal-popover-prototype');
+        popover.addClass('modal-popover ' + element.data('source-id'));
+        popover.css({
+            top: element.offset().top + element.height()
+        });
+        popover.html(source.html());
+        popover.hide();
+        
+        $('body').append(popover);
+        
+        // now that it's appended, we can get the width, set the left offset & show
+        popover.css({
+            left: element.offset().left - (popover.width() / 2)
+        })
+        popover.show();
     }
 } 
